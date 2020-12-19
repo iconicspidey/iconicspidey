bd404f64718f7163e2b3e981e1271eddb88bf4a7
iconicspidey <64977181+iconicspidey@users.noreply.github.com>
2020-12-20 00:35:53 +0100

    this is my first commit
    my first web page

---
+++ main.js
@@ -0,0 +1,16 @@
+// first page the image and the text container 
+
+let text1 = document.querySelector('.text1');
+let about = document.querySelector('.about');
+let story = document.querySelector('.story');
+
+window.addEventListener('load' , (e) => {
+    text1.style.transform = `translate(0%)`;
+
+    
+    
+    
+  
+}) 
+
+let 
